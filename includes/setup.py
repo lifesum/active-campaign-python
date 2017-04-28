5747b0d390e33dcd0610e1a5783d406af1dc5f29
michael-o-leary <maoleary92+github@gmail.com>
2017-04-28 14:03:07 -0500

    Create setup.py

---
+++ includes/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='ActiveCampaign',
+    version='0.0.1',
+    author='mo',
+    packages=['includes'],
+    description='ActiveCampaign python API.',
+    long_description=open('README.md').read()
+)
