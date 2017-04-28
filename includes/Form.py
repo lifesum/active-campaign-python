6e1a1f6410b05660ea33a7bb2c6581704d4a6134
michael-o-leary <maoleary92+github@gmail.com>
2017-04-28 14:43:02 -0500

    Update Form.py

--- includes/Form.py
+++ includes/Form.py
@@ -1,7 +1,7 @@
 from includes.Config import ACTIVECAMPAIGN_URL, ACTIVECAMPAIGN_API_KEY
 from includes.ActiveCampaign import ActiveCampaign
 import json
-import urllib2, urllib
+import urllib
 
 class Form(ActiveCampaign):
 
@@ -12,13 +12,13 @@ class Form(ActiveCampaign):
 
     def getforms(self, params, post_data = {}):
         request_url = '%s&api_action=form_getforms&api_output=%s' % (self.url, self.output)
-        response = json.loads(urllib2.urlopen(request_url).read())
+        response = json.loads(urllib.request.urlopen(request_url).read())
         return response
         
     def html(self, params, post_data = {}):
         request_url = '%s&api_action=form_html&api_output=%s&%s' % (self.url, self.output, params)
-        #print urllib2.urlopen(request_url).read()
-        response = json.loads(urllib2.urlopen(request_url).read())
+        #print urllib.request.urlopen(request_url).read()
+        response = json.loads(urllib.request.urlopen(request_url).read())
         return response
         
 if __name__ == '__main__':
