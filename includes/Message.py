c170702bef9fbd3db0fa43005465c24938b6a7bc
michael-o-leary <maoleary92+github@gmail.com>
2017-04-28 14:45:29 -0500

    Update Message.py

--- includes/Message.py
+++ includes/Message.py
@@ -1,8 +1,7 @@
-
 from includes.Config import ACTIVECAMPAIGN_URL, ACTIVECAMPAIGN_API_KEY
 from includes.ActiveCampaign import ActiveCampaign
 import json
-import urllib2, urllib
+import urllib
 import datetime
 
 class Message(ActiveCampaign):
@@ -15,81 +14,81 @@ class Message(ActiveCampaign):
     def add(self, params, post_data):
         request_url = '%s&api_action=message_add&api_output=%s' % (self.url, self.output)
         post_data = urllib.urlencode(post_data)
-        req = urllib2.Request(request_url, post_data)
-        response = json.loads(urllib2.urlopen(req).read())
+        req = urllib.request.Request(request_url, post_data)
+        response = json.loads(urllib.request.urlopen(req).read())
         return response
 
     def delete_list(self, params, post_data = {}):
         request_url = '%s&api_action=message_delete_list&api_output=%s&%s' % (self.url, self.output, params)
-        response = json.loads(urllib2.urlopen(request_url).read())
+        response = json.loads(urllib.request.urlopen(request_url).read())
         return response
 
     def delete(self, params, post_data = {}):
         request_url = '%s&api_action=message_delete&api_output=%s&%s' % (self.url, self.output, params)
-        response = json.loads(urllib2.urlopen(request_url).read())
+        response = json.loads(urllib.request.urlopen(request_url).read())
         return response
 
     def edit(self, params, post_data):
         request_url = '%s&api_action=message_edit&api_output=%s' % (self.url, self.output)
         post_data = urllib.urlencode(post_data)
         req = urllib2.Request(request_url, post_data)
-        response = json.loads(urllib2.urlopen(req).read())
+        response = json.loads(urllib.request.urlopen(req).read())
         return response
 
     def list_(self, params, post_data = {}):
         request_url = '%s&api_action=message_list&api_output=%s&%s' % (self.url, self.output, params)
-        response = json.loads(urllib2.urlopen(request_url).read())
+        response = json.loads(urllib.request.urlopen(request_url).read())
         return response
 
     def template_add(self, params, post_data):
         request_url = '%s&api_action=message_template_add&api_output=%s' % (self.url, self.output)
         post_data = urllib.urlencode(post_data)
         req = urllib2.Request(request_url, post_data)
-        response = json.loads(urllib2.urlopen(req).read())
+        response = json.loads(urllib.request.urlopen(req).read())
         return response
 
     def template_delete_list(self, params, post_data = {}):
         request_url = '%s&api_action=message_template_delete_list&api_output=%s&%s' % (self.url, self.output, params)
-        response = json.loads(urllib2.urlopen(request_url).read())
+        response = json.loads(urllib.request.urlopen(request_url).read())
         return response
 
     def template_delete(self, params, post_data = {}):
         request_url = '%s&api_action=message_template_delete&api_output=%s&%s' % (self.url, self.output, params)
-        response = json.loads(urllib2.urlopen(request_url).read())
+        response = json.loads(urllib.request.urlopen(request_url).read())
         return response
 
     def template_edit(self, params, post_data):
         request_url = '%s&api_action=message_template_edit&api_output=%s' % (self.url, self.output)
         post_data = urllib.urlencode(post_data)
         req = urllib2.Request(request_url, post_data)
-        response = json.loads(urllib2.urlopen(req).read())
+        response = json.loads(urllib.request.urlopen(req).read())
         return response
 
     def template_export(self, params, post_data = {}):
         request_url = '%s&api_action=message_template_export&api_output=%s&%s' % (self.url, self.output, params)
-        response = json.loads(urllib2.urlopen(request_url).read())
+        response = json.loads(urllib.request.urlopen(request_url).read())
         return response
 
     def template_import(self, params, post_data):
         request_url = '%s&api_action=message_template_import&api_output=%s' % (self.url, self.output)
         post_data = urllib.urlencode(post_data)
         req = urllib2.Request(request_url, post_data)
-        response = json.loads(urllib2.urlopen(req).read())
+        response = json.loads(urllib.request.urlopen(req).read())
         return response
 
     def template_list(self, params, post_data = {}):
         request_url = '%s&api_action=message_template_list&api_output=%s&%s' % (self.url, self.output, params)
-        response = json.loads(urllib2.urlopen(request_url).read())
+        response = json.loads(urllib.request.urlopen(request_url).read())
         return response
 
     def template_view(self, params, post_data = {}):
         request_url = '%s&api_action=message_template_view&api_output=%s&%s' % (self.url, self.output, params)
-        response = json.loads(urllib2.urlopen(request_url).read())
+        response = json.loads(urllib.request.urlopen(request_url).read())
         return response
 
     def view(self, params, post_data = {}):
         request_url = '%s&api_action=message_view&api_output=%s&%s' % (self.url, self.output, params)
-        response = json.loads(urllib2.urlopen(request_url).read())
+        response = json.loads(urllib.request.urlopen(request_url).read())
         return response
 
 
